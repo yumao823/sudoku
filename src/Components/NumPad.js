2f0f1dc5c4234edb6ed191bb0e3b0b96ec0edace
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-17 08:38:55 +0900

    feat: add iniital numpad component

---
+++ src/Components/NumPad.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const NumPad = () => {
+  return (
+    <div>
+      <Button>1</Button>
+    </div>
+  )
+}
+
+export default NumPad
