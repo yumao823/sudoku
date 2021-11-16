e1273478bd5fa45b29b02a9de261ba3d84df688a
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-17 08:38:17 +0900

    feat: add bootstrap css file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
