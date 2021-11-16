03382e07dd5fde87d2496050d33d6a8c88efe5dc
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-17 08:38:01 +0900

    feat: clean up code bases

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap'
 import './App.css';
 
 const App = () => {
@@ -17,20 +17,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>1</Button>
     </div>
   );
 }
