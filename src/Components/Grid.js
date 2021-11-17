029c65a566110f1b18813f925e82ba09a67db826
Koichi Noda <koichi.p.noda@gmail.com>
2021-11-17 12:18:40 +0900

    feat: clean up code bases

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const gridoptions = [
+const initialgridoptions = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -12,19 +12,31 @@ const gridoptions = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9],
 ]
 
-const GridItem = ({ grid }) => (
-  <div className="grid_container">
-    {grid.map((item, index) => (
-      <div className="grid_item py-2" key={`g-${index}`}>{item}</div>
-    ))}
-  </div>
-)
+const GridItem = ({ grid, index }) => {
+  const handleGridItem = idx => {
+    console.log(index, " : ", idx)
+  }
+  return (
+    <div className="grid_container">
+      {grid.map((item, idx) => (
+        <div
+          className="grid_item py-2" key={`g-${idx}`}
+          onClick={() => handleGridItem(idx)}
+        >
+          {item}
+        </div>
+      ))}
+    </div>
+  )
+}
 
 const Grid = () => {
+  const [gridOptions, setGridOptions] = useState(initialgridoptions)
+
   return (
     <div className="grid_container mb-3">
-      {gridoptions.map((grid, index) => (
-        <GridItem key={`gi-${index}`} grid={grid} />
+      {gridOptions.map((grid, index) => (
+        <GridItem key={`gi-${index}`} grid={grid} index={index} />
       ))}
     </div>
   )
